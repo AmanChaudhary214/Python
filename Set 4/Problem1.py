cb5bb26e8660877fd66217138fd163926ec637dd
Aman Chaudhary <aman.chaudhary214@gmail.com>
2023-08-07 11:03:13 +0530

    day 4 added

--- Set 4/Problem1.py
+++ Set 4/Problem1.py
@@ -4,4 +4,14 @@ Input: "cinema", "iceman"
 Output: "True"
 """
 
+str1 = "cinema"
+str2 = "iceman"
 
+def is_anagram(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
+    
+    return sorted(str1) == sorted(str2)
+
+result = is_anagram(str1, str2)
+print(result)  
